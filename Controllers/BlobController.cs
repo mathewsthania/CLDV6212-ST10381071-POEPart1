63742d32ec9ab92d7f8d983a92c6d4bfcb8ab31c
mathewsthania <ST10381071@VCCONNECT.EDU.ZA>
2024-08-29 00:50:15 +0200

    Adding in code for the blob and file controller classes.

--- Controllers/BlobController.cs
+++ Controllers/BlobController.cs
@@ -1,12 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CLDV6212_ST10381071_POEPart1.Services;
+using Microsoft.AspNetCore.Mvc;
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<//
 
 namespace CLDV6212_ST10381071_POEPart1.Controllers
 {
     public class BlobController : Controller
     {
-        public IActionResult Index()
+        private readonly BlobService _blobService; // creating instance of the blob service class
+
+        // creating constructor 
+        public BlobController(BlobService blobService)
         {
+            _blobService = blobService;
+        }
+
+        // action created to upload a form - to upload an image
+        [HttpGet]
+        public IActionResult UploadImage()
+        {
+            return View();
+        }
+
+        // action created to handle a form submission and to upload the image file to the blob storage
+        [HttpPost]
+        public async Task<IActionResult> UploadImage(IFormFile file)
+        {
+            // checking if the selected file is empty
+            if (file != null && file.Length > 0)
+            {
+                using (var stream = file.OpenReadStream())
+                {
+                    await _blobService.UploadBlobAsync("product-images", file.FileName, stream);
+                }
+
+                // if the file is uploaded - sucess message is displayed
+                ViewBag.Message = "File uploaded successflly!";
+            }
             return View();
         }
     }
 }
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*THE*END*OF*FILE*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<//
