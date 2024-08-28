379901b950deec2a94447868f12c8a152c416d4c
mathewsthania <ST10381071@VCCONNECT.EDU.ZA>
2024-08-29 01:43:01 +0200

    Adding code for the Queue and Table Controller - aswell as comments.

--- Controllers/TableController.cs
+++ Controllers/TableController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CLDV6212_ST10381071_POEPart1.Models;
+using CLDV6212_ST10381071_POEPart1.Services;
+using Microsoft.AspNetCore.Mvc;
 
 //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<//
 
@@ -6,9 +8,37 @@ namespace CLDV6212_ST10381071_POEPart1.Controllers
 {
     public class TableController : Controller
     {
-        public IActionResult Index()
+        
+        private readonly TableService _tableService; // creating instance of the table service class
+
+        // creating constructor 
+        public TableController(TableService tableService)
+        {
+            _tableService = tableService;
+        }
+
+        // method displays the dorm for adding a new customer profile
+        [HttpGet]
+        public IActionResult AddCustomerProfile()
         {
             return View();
+        }
+
+        // handles the form submission - for adding a new customer profile
+        [HttpPost]
+        public async Task<IActionResult> AddCustomerProfile(customerProfile profile)
+        {
+            if (ModelState.IsValid)
+            {
+                // adding customer profile to the table using table service storage
+                await _tableService.AddEntityAsync(profile);
+                
+                // displaying a success message - if it uploads successfully
+                ViewBag.Message = "Customer profile added successfully!";
+            }
+
+            // returns the view with the profile data that was inputted - if it was added succesfully or not
+            return View(profile);
         }
     }
 }
